6612f717aacd340d045bc049b9f76eafff7bb80e
Gigi M <mmgamao1@gmail.com>
2019-07-25 15:37:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,11 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change	  
     create_table :costumes do |t|
       t.string :name
+      t.string :price
+      t.string :image_url
+      t.integer :size  
+      t.datetime :date  
+      t.datetime :date  
     end
   end
 end
