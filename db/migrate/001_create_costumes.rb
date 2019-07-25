300392f62719b40d5ba020cf6b47a2ea3ae933b3
Gigi M <mmgamao1@gmail.com>
2019-07-25 15:41:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,6 +9,7 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change 
     create_table :costumes do |t|
       t.string :name
       t.string :size
