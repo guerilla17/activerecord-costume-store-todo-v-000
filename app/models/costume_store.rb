80660a4f1a1d2f4f15c22fda9fce79af79cbaceb
Gigi M <mmgamao1@gmail.com>
2019-07-25 15:32:02 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,18 +1,3 @@
 # Create your CostumeStore class here
-
-
-class CreateCostumeStores < ActiveRecord::Migration[4.2]
-  def change
-    create_table :costume_stores do |t|
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
-
-      t.timestamps null: false
-    end
-  end
+class Costume < ActiveRecord::Base
 end
